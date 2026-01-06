180193c92696c1387f9977eb8e3bc4ddc7372184
Sidou <takilaislife@gmail.com>
2026-01-06 14:27:05 +0100

    Update dbConnection.js

--- Ecommerce App-BackEnd/config/dbConnection.js
+++ Ecommerce App-BackEnd/config/dbConnection.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 async function dbconnection(dbname) {
     try {
-        await connect(`mongodb://127.0.0.1:27017/${dbname}`);
+        await connect(`mongodb+srv://ssidouuslm_db_user:Lu4tHKqJVTQqJZwW@cluster0.k0tnw42.mongodb.net/${dbname}`);
         console.log(`You are connected to the DATABASE`)
     } catch(error) { 
         console.error(error);
